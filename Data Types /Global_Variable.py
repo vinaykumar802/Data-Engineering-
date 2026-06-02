8a011bc32b984477f33b4eecd0ed1b5cb8d7f873
Fighter <fighter@kali.lan>
2026-06-02 21:51:59 +0530

    initial commit

---
+++ Data Types /Global_Variable.py
@@ -0,0 +1,12 @@
+# def myfunc():
+#     global x
+#     x = "Vinay kumar"
+# myfunc()
+# print("Python developer is " + x)
+x="2"
+def myfunc():
+    
+    x = "Vinay kumar"
+    print("Python developer is " + x)
+myfunc()
+print("Python developer is " + x)
